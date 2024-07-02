769e5392f786ea970b69e2f0cbf72b1bbf95100d
mecrophagus <122439515+mecrophagus@users.noreply.github.com>
2024-07-02 18:25:40 +0000

    Task.jsx

---
+++ src/js/component/Task.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Task = () => {
+
+
+    return (
+        
+        <div className="container">
+            <p>Hola soy task</p>
+            <ul>
+                <input type="text" />
+                <li>landing page whit react</li>
+                <li>A navbar component</li>
+                <li>Connect some api´s</li>
+                <li>Take time for your own project </li>
+                <li>Take some momment to read documentation </li>
+                <li>Keep taking hits by the code </li>
+            </ul>
+        </div>
+    )
+
+}
